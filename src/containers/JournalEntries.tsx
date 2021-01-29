e150fb50a4657961807d1081543f34da1f187536
Doug Kent <doug.kent88@gmail.com>
2021-01-29 13:05:51 -0600

    feat(auth): Adding authentication

---
+++ src/containers/JournalEntries.tsx
@@ -0,0 +1,11 @@
+// React
+import React from 'react';
+
+// Material UI
+import { Typography } from '@material-ui/core';
+
+const JournalEntries: React.FC = () => {
+    return <Typography variant='h3'>Journal Entries</Typography>;
+};
+
+export default JournalEntries;
